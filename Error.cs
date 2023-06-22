c4067044849109b62477fd173e9b8853baddb433
BG <BG@DESKTOP-AQPEC83>
2023-06-22 04:26:30 +0300

    Add project files.

---
+++ Error.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PASPAS
+{
+    public partial class Error : Form
+    {
+        public Error()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
