725842d2501668499bd51e8793a134a363166b05
Berkay Gediz <121058050+berkaygediz@users.noreply.github.com>
2023-09-14 20:26:50 +0300

    added rejected items, scaling changed to dpi.

--- Error.Designer.cs
+++ Error.Designer.cs
@@ -34,9 +34,10 @@
             this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Exit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Exit.ForeColor = System.Drawing.Color.White;
-            this.Exit.Location = new System.Drawing.Point(314, 55);
+            this.Exit.Location = new System.Drawing.Point(419, 68);
+            this.Exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(42, 42);
+            this.Exit.Size = new System.Drawing.Size(56, 52);
             this.Exit.TabIndex = 14;
             this.Exit.Text = "X";
             this.Exit.UseVisualStyleBackColor = false;
@@ -47,9 +48,10 @@
             this.Error_context.AutoSize = true;
             this.Error_context.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Error_context.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.Error_context.Location = new System.Drawing.Point(45, 70);
+            this.Error_context.Location = new System.Drawing.Point(60, 86);
+            this.Error_context.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Error_context.Name = "Error_context";
-            this.Error_context.Size = new System.Drawing.Size(196, 28);
+            this.Error_context.Size = new System.Drawing.Size(242, 34);
             this.Error_context.TabIndex = 13;
             this.Error_context.Text = "We don\'t know what happened either.\r\nPlease report the issue to us on GitHub.";
             // 
@@ -59,25 +61,27 @@
             this.Error_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Error_label.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Error_label.ForeColor = System.Drawing.Color.White;
-            this.Error_label.Location = new System.Drawing.Point(45, 38);
+            this.Error_label.Location = new System.Drawing.Point(60, 47);
+            this.Error_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Error_label.Name = "Error_label";
-            this.Error_label.Size = new System.Drawing.Size(149, 23);
+            this.Error_label.Size = new System.Drawing.Size(182, 30);
             this.Error_label.TabIndex = 12;
             this.Error_label.Text = "An Error Occurred!";
             // 
             // Error
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(400, 153);
+            this.ClientSize = new System.Drawing.Size(533, 188);
             this.ControlBox = false;
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.Error_context);
             this.Controls.Add(this.Error_label);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Error";
