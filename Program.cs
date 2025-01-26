43c49d5dba9298edb2983f6abb0771e49bc2c776
berkaygediz <121058050+berkaygediz@users.noreply.github.com>
2025-01-27 01:36:15 +0300

    code simplification, threading improvements, accessibility definitions of variables, application version issue fixed, unused old definitions deleted, PowerShell SDK update

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace PASPAS
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
         }
